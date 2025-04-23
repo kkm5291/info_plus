e8d17006be1e44147dffdc8c4e902b7a25992ca4
kkm5291 <kkm23125291@gmail.com>
2025-04-23 23:50:06 +0900

    [feat] ProfileRepository 추가 및 Test 코드 작성 - Profile 엔티티 (equals hashcode 추가)

--- src/main/java/com/example/infoplus/profile/domain/Profile.java
+++ src/main/java/com/example/infoplus/profile/domain/Profile.java
@@ -3,11 +3,20 @@ package com.example.infoplus.profile.domain;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 public class Profile {
 
     protected Profile() {}
+
+    protected Profile(String userName) {
+        this.userName = userName;
+    }
+
+    public static Profile of(String userName) {
+        return new Profile(userName);
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,6 +29,17 @@ public class Profile {
     private Long viewCount = 0L;
 
     @Column(nullable = false, updatable = false)
-    private LocalDateTime createdDate;
+    private LocalDateTime createdDate = LocalDateTime.now();
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Profile profile = (Profile) o;
+        return Objects.equals(id, profile.id) && Objects.equals(userName, profile.userName) && Objects.equals(viewCount, profile.viewCount) && Objects.equals(createdDate, profile.createdDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userName, viewCount, createdDate);
+    }
 }
---
+++ src/test/java/com/example/infoplus/profile/repository/ProfileRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.example.infoplus.profile.repository;
+
+import com.example.infoplus.profile.domain.Profile;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class ProfileRepositoryTest {
+
+    Profile profile;
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @BeforeEach
+    void setUp() {
+        profile = Profile.of("KIM");
+    }
+
+    @Test
+    void testSaveAndFindProfile() {
+        profileRepository.save(profile);
+
+        Profile findProfile = profileRepository.findById(1L)
+                .orElseThrow(() -> new IllegalArgumentException("Profile not found"));
+
+        assertThat(profile).isEqualTo(findProfile);
+    }
+
+    @Test
+    void testFindProfile_Error() {
+        assertThatThrownBy(() -> profileRepository.findById(1L).orElseThrow(() -> new IllegalArgumentException("Profile not found")));
+    }
+}
