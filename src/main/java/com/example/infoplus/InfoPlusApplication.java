25b484c60f843bd12029fdd6b06dd46a52abc822
kkm5291 <kkm23125291@gmail.com>
2025-04-23 20:56:16 +0900

    [chore] 초기 세팅 완료

---
+++ src/main/java/com/example/infoplus/InfoPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.infoplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfoPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/infoplus/InfoPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infoplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
