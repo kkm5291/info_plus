b3fc11bb4513f3e2b60184be5b636bafff9125f3
kkm5291 <kkm23125291@gmail.com>
2025-04-24 00:13:48 +0900

    [feat] ProfileService 추가 및 테스트 RED 코드 추가

---
+++ src/test/java/com/example/infoplus/profile/service/ProfileServiceTest.java
@@ -0,0 +1,34 @@
+package com.example.infoplus.profile.service;
+
+import com.example.infoplus.profile.domain.Profile;
+import com.example.infoplus.profile.repository.ProfileRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfileServiceTest {
+
+    @Mock
+    private ProfileRepository profileRepository;
+    private ProfileService profileService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        profileService = new ProfileServiceImpl(profileRepository);
+    }
+
+    @Test
+    void getProfile() {
+        BDDMockito.given(profileRepository.findById(1L)).willReturn(Optional.of(Profile.of("KIM")));
+
+        Profile profile = profileService.getProfile();
+        assertNotNull(profile);
+    }
+}
